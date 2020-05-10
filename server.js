11be4227646b324f01717f8eda9f7dd699fc34db
benjaminkan <50194133+benjaminkan@users.noreply.github.com>
2020-05-10 14:28:48 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
 const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
-var bodyParser = require("body-parser");
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(express.static('pages'));
 
 // Connect to mongodb
